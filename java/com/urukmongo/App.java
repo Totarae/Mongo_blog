939f1e9b72d91e689f1caf6dfca4cd23c53d9bb0
Unknown <gregoryivanoff6@gmail.com>
2019-12-16 12:26:55 +0300

    Clean things up

--- java/com/urukmongo/App.java
+++
@@ -1,8 +0,0 @@
-package com.urukmongo;
-
-/**
- * Created by Admin on 27.03.2017.
- */
-public class App {
-    public static void main(String[] args){System.out.println("Hello world!");};
-}
--- java/util/Path.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-/**
- * Created by Admin on 13.06.2018.
- */
-public class Path {
-    private final static String INDEX = "hello.html";
-}
