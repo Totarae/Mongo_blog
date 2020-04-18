513bf716eab5d78805c79c24a0de2022eaf44609
Totarae <gregoryivanoff6@gmail.com>
2020-04-18 22:51:03 +0300

    Authentication added

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,14 +1,29 @@
 package service;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import model.Blog;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class UserService {
-        MongoClient client = new MongoClient("185.93.109.227", 27017);
+
+        String user = "oooo";     // the user name
+        String source = "oooo";   // the source where the user is defined
+        String realPsswd = "oooo";
+
+        char[] password = realPsswd.toCharArray(); // the password as a character array
+
+        MongoCredential credential = MongoCredential.createCredential(user, source, password);
+
+        MongoClient client = new MongoClient(new ServerAddress("000.000.000.000", 27017), Collections.singletonList(credential));
+
 
         Datastore datastore = new Morphia().createDatastore(client, "blog");
 
